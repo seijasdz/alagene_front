f8477bd7cd4c0ad4aec99c39b1cfdf1d301727a7
José Rául Hernández Seijas <seijasdz@gmail.com>
2020-02-28 07:23:21 -0400

    corrected gff

--- app/helpers/gffSerializer.js
+++ app/helpers/gffSerializer.js
@@ -19,7 +19,7 @@ const makeRNAFeature = (seq_id, number, rna) => {
     rna.ss[0],
     rna.ss[1],
     null,
-    { ID: ['rna' + number] },
+    { ID: ['rna' + number], parent: ['gene' + number] },
   );
 };
 
@@ -46,19 +46,23 @@ const makeGff = (pre) => {
   let geneC = 0;
   let cdsC = 0;
   let exonC = 0;
+  let binC = 0;
 
   if (pre.result && pre.result.genes) {
     for (const gene of pre.result.genes) {
       features.push(makeGeneFeature(pre.seq_id, geneC, gene));
       features.push(makeRNAFeature(pre.seq_id, geneC, gene)); // gene == mRNA
       geneC += 1;
-      
+      for (const binding of gene.binding) {
+        features.push(makeFeature(pre.seq_id, 'TF_binding_site', binding, binding + 10, null, {ID: 'tfbs' + binC, parent: 'gene' + (geneC - 1)}))
+        binC += 1
+      }
       for (const ex of gene.exon) {
-        features.push(makeFeature(pre.seq_id, 'exon', ex[0], ex[1], null, {ID: 'exon' + exonC}));
+        features.push(makeFeature(pre.seq_id, 'exon', ex[0], ex[1], null, {ID: 'exon' + exonC, parent: 'mRNA' + (geneC - 1)}));
         exonC += 1
       }
       for (const cds of gene.cds) {
-        features.push(makeFeature(pre.seq_id, 'cds', cds[0], cds[1], 0, {ID: 'cds' + cdsC}));
+        features.push(makeFeature(pre.seq_id, 'cds', cds[0], cds[1], 0, {ID: 'cds' + cdsC, parent: 'rna' + (geneC - 1)}));
         cdsC += 1
       }
     } 
